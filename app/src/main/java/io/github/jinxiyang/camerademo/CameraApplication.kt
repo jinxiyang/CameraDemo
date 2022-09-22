9ea02856701e3855adc268a721ae50d93e3e3f33
yjx <jinxi.yang@bl.com>
2022-09-22 20:30:49 +0800

    preview

---
+++ app/src/main/java/io/github/jinxiyang/camerademo/CameraApplication.kt
@@ -0,0 +1,15 @@
+package io.github.jinxiyang.camerademo
+
+import android.app.Application
+import android.util.Log
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
+
+class CameraApplication : Application(), CameraXConfig.Provider {
+
+
+
+    override fun getCameraXConfig(): CameraXConfig {
+        return CameraXConfig.Builder.fromConfig(Camera2Config.defaultConfig()).setMinimumLoggingLevel(Log.INFO).build()
+    }
+}
--- app/src/main/java/io/github/jinxiyang/camerademo/MainActivity.kt
+++ app/src/main/java/io/github/jinxiyang/camerademo/MainActivity.kt
@@ -1,11 +1,85 @@
 package io.github.jinxiyang.camerademo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import io.github.jinxiyang.requestpermission.PermissionRequester
+import io.github.jinxiyang.requestpermission.utils.PermissionUtils
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var previewView: PreviewView
+
+
+    private var initCamera = false
+    private var requestCameraPermission = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        previewView = findViewById(R.id.previewView)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (PermissionUtils.hasPermissions(this, PermissionUtils.CAMERA_PERMISSIONS)) {
+            initCamera(this, this, previewView)
+        } else if (!requestCameraPermission){
+            requestCameraPermission = true
+            PermissionRequester(this)
+                .addPermissions(PermissionUtils.CAMERA_PERMISSIONS)
+                .request {
+                    if (it.granted()) {
+                        initCamera(this, this, previewView)
+                    } else {
+                        Toast.makeText(this@MainActivity, "没有相机权限", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+
+
+    fun initCamera(context: Context, lifecycleOwner: LifecycleOwner, previewView: PreviewView) {
+        if (initCamera) {
+            return
+        }
+        initCamera = true
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+        cameraProviderFuture.addListener({
+           val cameraProvider = cameraProviderFuture.get()
+
+
+            val preview = Preview.Builder().build()
+
+            val imageCapture = ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build()
+
+            val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+
+            val camera = cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageCapture)
+
+            preview.setSurfaceProvider(previewView.surfaceProvider)
+
+            val controller = LifecycleCameraController(this)
+            controller.bindToLifecycle(this)
+            previewView.controller = controller
+        }, ContextCompat.getMainExecutor(context))
     }
 }
